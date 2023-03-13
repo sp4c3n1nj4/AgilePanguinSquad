461cfd4ecf220bea09dc79e3503a14861e9005dd
sp4c3n1nj4 <paulinus-erhardt@gmx.net>
2023-03-13 17:30:34 +0000

    procedural anim improvements
    look at exporting logic to seperate bluepirnt to enable access

--- Project/250223/Experimental/Source/Experimental/ExperimentalCharacter.cpp
+++ Project/250223/Experimental/Source/Experimental/ExperimentalCharacter.cpp
@@ -40,7 +40,7 @@ AExperimentalCharacter::AExperimentalCharacter()
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 0.0f, 100.0f); // ...at this rotation rate
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
